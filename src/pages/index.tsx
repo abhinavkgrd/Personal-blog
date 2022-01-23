63dfd7996aece2a5ff3b1d2fc0a29fad9068f12b
abhinavkgrd <abhinavk.grd@gmail.com>
2022-01-23 15:18:40 +0530

    temp put cooking gif on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
-
+import { graphql } from 'gatsby'
+import CookingGIF from 'images/cooking.gif'
 import Bio from 'components/bio'
 import Layout from 'components/layout'
 import Seo from 'components/seo'
@@ -8,13 +9,51 @@ const BlogIndex = ({ data, location }) => {
     const siteTitle = data.site.siteMetadata?.title || `Title`
 
     return (
-        <Layout location={location} title={siteTitle}>
-            <Seo title="Home" />
+        <Layout location={location} title={'learnings'}>
+            <Seo title="learnings" />
             <Bio />
 
             <hr />
+
+            <div
+                style={{
+                    display: 'flex',
+                    textAlign: 'center',
+                    justifyContent: 'center',
+                    flex: '1',
+                    flexDirection: 'column',
+                }}>
+                <h4>Something good is cooking 😋</h4>
+                <p>please be patient 😌 </p>
+                <div style={{ fontSize: '100px' }}>
+                    <img src={CookingGIF} alt="cooking gif" />
+                </div>
+            </div>
         </Layout>
     )
 }
 
 export default BlogIndex
+
+export const pageQuery = graphql`
+    query {
+        site {
+            siteMetadata {
+                title
+            }
+        }
+        allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+            nodes {
+                excerpt
+                fields {
+                    slug
+                }
+                frontmatter {
+                    date(formatString: "MMMM DD, YYYY")
+                    title
+                    description
+                }
+            }
+        }
+    }
+`
